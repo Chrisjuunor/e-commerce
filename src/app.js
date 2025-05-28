a02fc85097e952af42aaacb89a891b1594c0fbdc
akachukwu <chinonsoigweic@gmail.com>
2025-05-28 09:19:01 +0100

    add product to cart

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { productRouter } from "./route/product.router.js";
 import { userRouter } from "./route/user.router.js";
+import { cartRouter } from "./route/cart.router.js";
 
 const app = express();
 app.use(express.json());
 
 app.use(userRouter);
 app.use(productRouter);
+app.use(cartRouter);
 
 export default app;
